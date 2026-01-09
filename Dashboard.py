96c432678b9658e1e01c98dc69aafdacba6a7100
Vivek Kumar Singh <vivekkumarsingh1811@gmail.com>
2026-01-09 16:45:17 +0530

    Second Commit

--- Dashboard.py
+++ Dashboard.py
@@ -10,7 +10,7 @@ st.sidebar.selectbox("Segment",['All','Enterprise','Mid-Market','SMB'])
 
 # Columns
 
-col1, col2, col3 = st.columns(3)
+col1, col2, col3,col4 = st.columns(4)
 
 with col1:
     st.subheader('Sales')
@@ -23,3 +23,7 @@ with col2:
 with col3:
     st.subheader('Partners')
     st.success('2500')
+
+with col4:
+    st.subheader('Customer')
+    st.success('1.1M')
